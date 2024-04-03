227d8d5f3b59b272ec416bb331dd8d2256ca5091
JuanDavidSerna <jserna@cloudlabs.co>
2024-04-03 10:36:10 -0500

    Todo-List

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from .models import Project, Task
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
-def hello(Request):
-    return HttpResponse("Hola django")
+def index(request):
+    title = 'Django practics!!'
+    return render(request,'index.html', {
+        'title': title
+    })
 
-def about(Request):
-    return HttpResponse("Holaaaaa")
+def about(request):
+    username ='juanDa'
+    return render(request,'about.html',{
+        'username':username
+    })
+
+def hello(request, username):
+    return HttpResponse("<h2>Hola Practicando Django %s</h2>" % username)
+
+def projects(request):
+    # projects = list(Project.objects.values())
+    projects = Project.objects.all()
+    return render(request,'projects.html',{
+        'projects': projects
+    })
+
+def tasks(request):
+    # task = get_object_or_404
+    tasks = Task.objects.all()
+    return render(request, 'tasks.html',{
+        'tasks': tasks 
+    })
